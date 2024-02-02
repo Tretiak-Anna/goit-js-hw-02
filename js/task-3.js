faf807af3d87ea49a95c22021af61bd7f4104a31
Tretiak-Anna <151523272+Tretiak-Anna@users.noreply.github.com>
2024-02-02 17:27:08 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  let contentWidth = parseFloat(content);
+  let paddingWidth = parseFloat(padding);
+  let borderWidth = parseFloat(border);
+  return contentWidth + (paddingWidth + borderWidth) * 2;
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
